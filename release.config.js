7d303da7ee0dab6e7ef829bd407a50d6432deaaa
Ryan Johnson <ryan@sprucelabs.ai>
2022-12-09 21:28:55 -0500

    patch: release branch

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ const spruceSemanticRelease = require('@sprucelabs/semantic-release')
 
 const config = spruceSemanticRelease.default({
 	npmPublish: false,
-	branches: [{ name: 'master', channel: 'latest' }],
+	branches: [{ name: 'main', channel: 'latest' }],
 	releaseMessage: 'chore(release): ${nextRelease.version} [npm-publish]',
 })
 
