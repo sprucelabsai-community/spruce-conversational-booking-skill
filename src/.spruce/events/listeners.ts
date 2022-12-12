164e308cd52a44f7ddad5929b2b551aad9132c9c
CircleCI <devops@sprucelabs.ai>
2022-12-12 16:29:33 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -1,15 +1,13 @@
 import { EventFeatureListener } from '@sprucelabs/spruce-event-utils'
 
 const listeners: EventFeatureListener[] = [
-	{
-		eventName: 'will-boot',
-		eventNamespace: 'skill',
-		version: 'v2022_12_09',
-		callback: require('../../listeners/skill/will-boot.v2022_12_09.listener')
-			.default,
-		isGlobal: require('../../listeners/skill/will-boot.v2022_12_09.listener')
-			.isGlobal,
-	},
+    {
+        eventName: 'will-boot',
+        eventNamespace: 'skill',
+        version: 'v2022_12_09',
+        callback: require('../../listeners/skill/will-boot.v2022_12_09.listener').default,
+        isGlobal: require('../../listeners/skill/will-boot.v2022_12_09.listener').isGlobal,
+    },
 ]
 
 export default listeners
